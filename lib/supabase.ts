e00b66fb741487a6cb697dea2a153f0e0ce26881
Dinfadashe <dinfadashe@gmail.com>
2026-04-26 14:08:38 +0100

    fix netlify config

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const anon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-const service = process.env.SUPABASE_SERVICE_ROLE_KEY!;
+const url = "https://dmfxgcouxbgvcomartrv.supabase.co";
+const anon = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRtZnhnY291eGJndmNvbWFydHJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcwODEzMzAsImV4cCI6MjA5MjY1NzMzMH0.UC0MmiIzv3yGNZ4OjRh0QmNYbhw--hfKJqIrxEBHSLw";
+const service = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRtZnhnY291eGJndmNvbWFydHJ2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NzA4MTMzMCwiZXhwIjoyMDkyNjU3MzMwfQ.D3_EnOMKHjwWlsidM8idjopGOSUwLowXcdOd0X5oIJ8";
 
 export const supabase = createClient(url, anon);
 
