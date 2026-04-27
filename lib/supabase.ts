5c6f9e258f2779b0768a19171f68a451eb01d0a3
Dinfadashe <dinfadashe@gmail.com>
2026-04-27 14:53:21 +0100

    force fresh build v2

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,5 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
+// v2
 const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const anon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 const service = process.env.SUPABASE_SERVICE_ROLE_KEY!;
